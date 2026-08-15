adb6786d721e8ad6cebd00f4f9952694ed1e6e0d
Ivas1981 <ivasinst@gmail.com>
2026-08-15 23:30:12 +0300

    feat: also collapse sampling widgets (temperature, max_tokens, repeat_penalty, top_k, top_p, min_p) under Advanced settings

--- web/js/llm_prompt_studio_actions.js
+++ web/js/llm_prompt_studio_actions.js
@@ -13,7 +13,9 @@ import {
 
 // The four model-load widgets that the "⚙ Advanced settings" button shows/hides.
 const ADVANCED_WIDGETS = ["context_length", "gpu_offload",
-                           "flash_attention", "offload_kv_cache_to_gpu"];
+                           "flash_attention", "offload_kv_cache_to_gpu",
+                           "temperature", "max_tokens", "repeat_penalty",
+                           "top_k", "top_p", "min_p"];
 
 // Hide/show a single widget named `name` on `node`.
 //
